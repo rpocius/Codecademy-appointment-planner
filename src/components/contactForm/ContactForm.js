70f16c4d71051f84bda2315c91ffd8c6a1bc2abc
Rai Pocius <pocius.raimondas@gmail.com>
2024-03-02 16:24:59 -0600

    Add state and functions

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -9,8 +9,35 @@ export const ContactForm = ({
   setEmail,
   handleSubmit
 }) => {
+
   return (
-    <></>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          name="name"
+          type="text"
+          value={name}
+          onChange={setName()}
+        />
+        <input
+          name="phone"
+          type="tel"
+          value={phone}
+          pattern="^(\(?\+?[0-9]*\)?)?[0-9_\- \(\)]*$"
+          onChange={setPhone()}
+        />
+        <input
+          name="email"
+          type="email"
+          value={email}
+          onChange={setEmail()}
+        />
+        <button
+          type="submit"
+        >Submit
+        </button>
+      </form>
+    </>
   );
 };
 
--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -3,29 +3,40 @@ import React, { useState, useEffect } from "react";
 import { ContactForm } from "../../components/contactForm/ContactForm";
 import { TileList } from "../../components/tileList/TileList";
 
-export const ContactsPage = () => {
-  /*
-  Define state variables for 
-  contact info and duplicate check
-  */
+export const ContactsPage = (props) => {
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [duplicateName, setDuplicateName] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    /*
-    Add contact info and clear data
-    if the contact name is not a duplicate
-    */
+    if (!duplicateName) {
+      props.addContact(name, phone, email);
+      setName('');
+      setPhone('');
+      setEmail('');
+    }
   };
 
-  /*
-  Using hooks, check for contact name in the 
-  contacts array variable in props
-  */
+  useEffect(() => {
+    const isDuplicate = props.contacts.some(contact => contact.name === name);
+    setDuplicateName(isDuplicate);
+  }, [name, props.contacts]);
 
   return (
     <div>
       <section>
-        <h2>Add Contact</h2> 
+        <h2>Add Contact</h2>
+        <ContactForm 
+          name={name}
+          setName={setName}
+          phone={phone}
+          setPhone={setPhone}
+          email={email}
+          setEmail={setEmail}
+          onSubmit={handleSubmit}
+        />
       </section>
       <hr />
       <section>
