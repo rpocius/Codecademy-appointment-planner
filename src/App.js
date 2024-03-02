a9e3b232529621883a6b54c7162675199681ed3c
Rai Pocius <pocius.raimondas@gmail.com>
2024-03-02 13:41:24 -0600

    Set up state and pass props to components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route, Navigate } from "react-router-dom"
 import Root, { ROUTES } from "./components/root/Root";
 import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
 import { ContactsPage } from "./containers/contactsPage/ContactsPage";
 
 function App() {
-  /*
-  Define state variables for 
-  contacts and appointments 
-  */
+  const [contacts, setContacts] = useState([]);
+  const [appointments, setAppointments] = useState([]);
 
-  /*
-  Implement functions to add data to
-  contacts and appointments
-  */
+  function addContact(name, phone, email) {
+    setContacts([
+      ...contacts,
+      {
+        name: name,
+        phone: phone,
+        email: email
+      }
+    ]);
+  }
+
+  function addAppointment(name, contact, date, time) {
+    setAppointments([
+      ...appointments,
+      {
+        name: name,
+        contact: contact,
+        date: date,
+        time: time
+      }
+    ]);
+  }
 
   const router = createBrowserRouter(createRoutesFromElements(
-    <Route path="/" element={ <Root/> }>
-      <Route index element={ <Navigate to={ROUTES.CONTACTS} replace/> }/>
-      <Route path={ROUTES.CONTACTS} element={ <ContactsPage /> /* Add props to ContactsPage */ }/>
-      <Route path={ROUTES.APPOINTMENTS} element={ <AppointmentsPage /> /* Add props to AppointmentsPage */ }/>
+    <Route path="/" element={<Root />}>
+      <Route index element={<Navigate to={ROUTES.CONTACTS} replace />} />
+      <Route path={ROUTES.CONTACTS} element={<ContactsPage contacts={contacts} addContact={addContact} />} />
+      <Route path={ROUTES.APPOINTMENTS} element={<AppointmentsPage appointments={appointments} addAppointment={addAppointment} />} />
     </Route>
   ));
-  
+
   return (
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   );
 }
 
