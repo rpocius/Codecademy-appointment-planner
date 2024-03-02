7053db74a076014501e31640bd93e04507b82538
Rai Pocius <pocius.raimondas@gmail.com>
2024-03-02 16:53:10 -0600

    Add renderTiles function

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,17 @@
 import React from "react";
+import Tile from '../../tile/tile'
 
-export const TileList = () => {
+export const TileList = (props) => {
+
+  function renderTiles(arr) {
+    return arr.map(({ name, ...rest }, index) => {
+      return <Tile name={name} description={rest} key={index} />
+    });
+  }
+
   return (
     <div>
-      
+
     </div>
   );
 };
